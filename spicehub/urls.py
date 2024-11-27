d7c20270c00919327963b960bfe536f1ed68d80d
Pooja Parmar <pooja.par92@gmail.com>
2024-11-27 12:50:40 +0000

    complete homepage header and css

--- spicehub/urls.py
+++ spicehub/urls.py
@@ -15,15 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-# Define the success view
-def success_view(request):
-    return HttpResponse("Welcome to the success page!")
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
