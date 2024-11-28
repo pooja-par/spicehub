4737f384d397efb91d779edbfe57479ae323ce11
Pooja Parmar <pooja.par92@gmail.com>
2024-11-28 01:58:47 +0000

    added detailed functionality

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),  # All products page
+    path('<slug:slug>/', views.product_detail, name='product_detail'),  # Product detail page
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
 
     context = {
@@ -13,3 +12,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, slug):
+    """ A view to show individual product details """
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
