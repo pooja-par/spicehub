b4918b636b29614f79d6031b197b86a8071427f7
Pooja Parmar <pooja.par92@gmail.com>
2024-11-28 02:26:43 +0000

    Update of product functionality and searchbar

--- products/views.py
+++ products/views.py
@@ -1,21 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-# Create your views here.
-
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """
+    A view to show all products, including sorting and search queries.
+    """
     products = Product.objects.all()
+    query = request.GET.get('q')
+    category = request.GET.get('category')
+    sort = request.GET.get('sort')
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if category:
+        products = products.filter(category__name=category)
+
+    if sort:
+        if sort == 'price':
+            products = products.order_by('price_per_kg')
+        elif sort == 'rating':
+            products = products.order_by('-rating')
 
     context = {
         'products': products,
+        'search_term': query,
+        'selected_category': category,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, slug):
-    """ A view to show individual product details """
+    """
+    A view to show individual product details by slug.
+    """
     product = get_object_or_404(Product, slug=slug)
 
     context = {
