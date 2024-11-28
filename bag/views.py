9e3420a17f5c91753511979387900c2a08c4b026
Pooja Parmar <pooja.par92@gmail.com>
2024-11-28 10:59:19 +0000

    update of product detail file

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,20 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, product_id):
+    """
+    Add a specified quantity of the product in kilograms to the shopping bag.
+    """
+    quantity = float(request.POST.get('quantity'))  # Allow decimal input for kg
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in bag:
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
