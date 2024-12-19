393614d26b43245af32f54865861bfa3eb7cf957
Pooja Parmar <pooja.par92@gmail.com>
2024-12-19 02:57:01 +0000

    form for the check out and input for credit card created

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'ppk_test_51QXZBkAwymsVDFhSqmmLdUPfNO1I1EGYMye4Nzz1HJL9fbdfhjgwWxvuQBU7rZWO7f0sc8aKISBSzJw5Mmduao7N00ZZiRpim1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
