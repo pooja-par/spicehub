d371542bec08a76d188bf317fa167139ebf8f516
Pooja Parmar <pooja.par92@gmail.com>
2024-12-19 11:15:43 +0000

    Product management

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products'),  # All products page
-    path('<slug:slug>/', views.product_detail, name='product_detail'),  # Product detail page
+    path('', views.all_products, name='products'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,44 +1,81 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
+from .forms import ProductForm
+
+# Create your views here.
 
 def all_products(request):
-    """
-    A view to show all products, including sorting and search queries.
-    """
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
-    query = request.GET.get('q')
-    category = request.GET.get('category')
-    sort = request.GET.get('sort')
+    query = None
+    categories = None
+    sort = None
+    direction = None
 
-    if query:
-        products = products.filter(name__icontains=query)
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-    if category:
-        products = products.filter(category__name=category)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
-    if sort:
-        if sort == 'price':
-            products = products.order_by('price_per_kg')
-        elif sort == 'rating':
-            products = products.order_by('-rating')
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
-        'selected_category': category,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request, slug):
-    """
-    A view to show individual product details by slug.
-    """
-    product = get_object_or_404(Product, slug=slug)
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
