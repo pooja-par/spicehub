a4ea13dad1fd9be550501475c2e079c839d6b4aa
Pooja Parmar <pooja.par92@gmail.com>
2025-08-01 01:02:04 +0000

    Format change for the main page

--- spicehub/settings.py
+++ spicehub/settings.py
@@ -47,11 +47,11 @@ DEBUG = True
 #]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-poojapar-spicehub-1l4e2zf7wgo.ws-eu120.gitpod.io',
+    'https://8000-poojapar-spicehub-shqdug72210.ws-eu120.gitpod.io',
     'https://your-heroku-app-name.herokuapp.com',  # Replace with your actual Heroku app URL
 ]
 ALLOWED_HOSTS = [
-    '8000-poojapar-spicehub-1l4e2zf7wgo.ws-eu120.gitpod.io',
+    '8000-poojapar-spicehub-shqdug72210.ws-eu120.gitpod.io',
     'spicehub-4df0a1a6c581.herokuapp.com',
     'localhost'
 ]
