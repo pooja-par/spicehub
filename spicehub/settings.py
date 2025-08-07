04315e7df282a66f29d1c04d3e4beeff3e0f1612
Pooja Parmar <pooja.par92@gmail.com>
2025-08-07 01:33:23 +0000

    Formating of the home page is changed

--- spicehub/settings.py
+++ spicehub/settings.py
@@ -47,11 +47,11 @@ DEBUG = True
 #]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-poojapar-spicehub-ivo8p2lvj34.ws-eu120.gitpod.io',
+    'https://8000-poojapar-spicehub-dw1io9yhte1.ws-eu120.gitpod.io',
     'https://your-heroku-app-name.herokuapp.com',  # Replace with your actual Heroku app URL
 ]
 ALLOWED_HOSTS = [
-    '8000-poojapar-spicehub-ivo8p2lvj34.ws-eu120.gitpod.io',
+    '8000-poojapar-spicehub-dw1io9yhte1.ws-eu120.gitpod.io',
     'spicehub-4df0a1a6c581.herokuapp.com',
     'localhost'
 ]
