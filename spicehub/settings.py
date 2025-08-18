e5f261412fa5f5a6f5aa686461b926a5b5b6142d
Pooja Parmar <pooja.par92@gmail.com>
2025-08-18 03:26:21 +0000

    nothing special, just trial

--- spicehub/settings.py
+++ spicehub/settings.py
@@ -80,7 +80,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,8 +90,8 @@ MIDDLEWARE = [
     
 ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ROOT_URLCONF = 'spicehub.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
