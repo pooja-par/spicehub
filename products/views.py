cef7a6cdf2d2ccbb7abda6addbb4d3cc666fb59b
pooja-par <pooja.par92@gmail.com>
2026-03-10 01:46:32 +0100

    error corrections

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.db.utils import OperationalError, ProgrammingError
 from django.db.models.functions import Lower
 
 from .models import Product, Category
@@ -17,34 +18,49 @@ def all_products(request):
     sort = None
     direction = None
 
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-            
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+    try:
+        if request.GET:
+            if 'sort' in request.GET:
+                sortkey = request.GET['sort']
+                sort = sortkey
+                if sortkey == 'name':
+                    sortkey = 'lower_name'
+                    products = products.annotate(lower_name=Lower('name'))
+                if sortkey == 'category':
+                    sortkey = 'category__name'
+                if 'direction' in request.GET:
+                    direction = request.GET['direction']
+                    if direction == 'desc':
+                        sortkey = f'-{sortkey}'
+                products = products.order_by(sortkey)
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+            if 'category' in request.GET:
+                categories = request.GET['category'].split(',')
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+
+            if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "You didn't enter any search criteria!")
+                    return redirect(reverse('products'))
+
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
+
+        # Force DB evaluation here so migration/table issues are caught and
+        # handled instead of raising a 500 while rendering templates.
+        products = list(products)
+        if categories is not None:
+            categories = list(categories)
+    except (OperationalError, ProgrammingError):
+        products = []
+        categories = []
+        messages.warning(
+            request,
+            'Products are temporarily unavailable while the database is initializing. '
+            'Please refresh in a moment.',
+        )
 
     current_sorting = f'{sort}_{direction}'
 
@@ -60,7 +76,18 @@ def all_products(request):
 
 def product_detail(request, product_slug):
     """A view to show individual product details"""
-    product = get_object_or_404(Product, slug=product_slug)
+    #product = get_object_or_404(Product, slug=product_slug)
+
+    try:
+        product = get_object_or_404(Product, slug=product_slug)
+    except (OperationalError, ProgrammingError):
+        messages.warning(
+            request,
+            'Product details are temporarily unavailable while the database is initializing. '
+            'Please refresh in a moment.',
+        )
+        return redirect(reverse('products'))
+    
     context = {'product': product}
     return render(request, 'products/product_detail.html', context)
 
