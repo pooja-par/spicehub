e1ffe28b8bab9dc3a02fb9c20a4d5ac8cbd46f16
pooja-par <pooja.par92@gmail.com>
2026-03-10 04:11:25 +0100

    Updating view of product and details

--- products/models.py
+++ products/models.py
@@ -26,9 +26,16 @@ class Product(models.Model):
     """
     Model representing a product.
     """
-    category = models.ForeignKey(
-        'Category',
-@@ -43,50 +44,62 @@ class Product(models.Model):
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL, related_name='products')
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    price_per_kg = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(null=True, blank=True, upload_to='media/')
+    #json_data = models.JSONField(default=dict)  # Additional metadata (e.g., origin, grade)
     low_stock_threshold = models.PositiveIntegerField(default=25, blank=True)
     critical_stock_threshold = models.PositiveIntegerField(default=5, blank=True)
     discount_rules = models.JSONField(
